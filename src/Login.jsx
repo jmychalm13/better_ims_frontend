09b9a6dc4d081189fa275cd0f41037bfef321881
“Jessica <jmychalm13@gmail.com>
2023-09-04 16:44:13 -0500

    redesigned login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -31,26 +31,41 @@ export function Login() {
 
   console.log(jwt);
   return (
-    <div id="login" className="wrapper">
-      <div className="text-center mt-4 name">Better IMS</div>
-      <ul>
-        {errors.map((error) => (
-          <li key={error}>{error}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit} className="p-3 mt-3">
-        <div className="form-field d-flex align-items-center">
-          <span className="far fa-user"></span>
-          Email: <input className="form-control" name="email" type="email" />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h2 className="text-center text-dark mt-5">Better IMS</h2>
+          <div className="text-center mb-5 text-dark">A better way to manage your inventory</div>
+          <div className="card my-5">
+            <form onSubmit={handleSubmit} className="card-body cardbody-color p-lg-5">
+              <ul>
+                {errors.map((error) => (
+                  <li key={error}>{error}</li>
+                ))}
+              </ul>
+              <div className="text-center">
+                <img
+                  src="src/assets/logo-transparent-png.png"
+                  alt=""
+                  className="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                  width="200px"
+                />
+              </div>
+              <div className="mb-3">
+                <input className="form-control" name="email" type="email" placeholder="Email" />
+              </div>
+              <div className="mb-3">
+                <input className="form-control" name="password" type="password" placeholder="password" />
+              </div>
+              <div className="text-center">
+                <button type="submit" className="btn btn-color mt-3">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="form-field d-flex align-items-center">
-          <span className="fas fa-key"></span>
-          Password: <input className="form-control" name="password" type="password" />
-        </div>
-        <button type="submit" className="btn mt-3">
-          Login
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
