c85e13f3f3d5c2f7cb9a08dfb616f79ca3dd0065
“Jessica <jmychalm13@gmail.com>
2023-09-04 09:35:18 -0500

    worked on login component

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,54 @@
 import axios from "axios";
+import { useState } from "react";
+
+const jwt = localStorage.getItem("jwt");
+
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+}
 
 export function Login() {
-  return <h1>I work</h1>;
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors([]);
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/sessions.json", params)
+      .then((response) => {
+        console.log("Log here", response.data);
+        axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.jwt}`;
+        localStorage.setItem("jwt", response.data.jwt);
+        event.target.reset();
+        window.location.href = "/"; // change this to hide a modal or go to a specific page
+      })
+      .catch((error) => {
+        console.log("Log here", error.response);
+        setErrors(["Invalid email or password"]);
+      });
+  };
+
+  console.log(jwt);
+  return (
+    <div id="login">
+      <h1>Login</h1>
+      <ul>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          Email: <input className="form-control" name="email" type="email" />
+        </div>
+        <div className="form-group">
+          Password: <input className="form-control" name="password" type="password" />
+        </div>
+        <button type="submit" className="btn btn-primary mt-3">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
