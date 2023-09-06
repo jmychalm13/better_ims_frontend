d63a950332c62c2a4f3c9ab638a4ecc3ef83207e
“Jessica <jmychalm13@gmail.com>
2023-09-05 20:53:11 -0500

    getting products from API

--- src/OrderCreate.jsx
+++ src/OrderCreate.jsx
@@ -1,4 +1,17 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+
 export function OrderCreate() {
+  const [products, setProducts] = useState([]);
+
+  const displayProducts = () => {
+    axios.get("http://localhost:3000/products.json").then((response) => {
+      setProducts(response.data);
+    });
+  };
+
+  useEffect(displayProducts, []);
   return (
     <div className="order-form">
       <form className="mt-5 pt-2 m-auto rounded">
@@ -17,36 +30,19 @@ export function OrderCreate() {
             <strong>Quantity</strong>
           </div>
         </div>
-        <div className="row">
-          <div className="product-name col border-bottom-0 text-center">
-            <p>Vanilla Syrup</p>
+        {products.map((product) => (
+          <div key={product.id} className="row">
+            <div className="product-name col border-bottom-0 text-center">
+              <p>{product.product_name}</p>
+            </div>
+            <div className="col text-center text-center uom">{product.uom}</div>
+            <div className="col text-center on-hand">{product.on_hand}</div>
+            <div className="col text-center">
+              <input type="number" className="form-control" placeholder="Quantity" />
+            </div>
           </div>
-          <div className="col text-center text-center uom">12</div>
-          <div className="col text-center on-hand">85</div>
-          <div className="col text-center">
-            <input type="number" className="form-control" placeholder="Quantity" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="product-name col text-center">
-            <p>Vanilla Syrup</p>
-          </div>
-          <div className="col text-center uom">12</div>
-          <div className="col text-center on-hand">85</div>
-          <div className="col text-center">
-            <input type="number" className="form-control" placeholder="Quantity" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="product-name col text-center">
-            <p>Vanilla Syrup</p>
-          </div>
-          <div className="col text-center uom">12</div>
-          <div className="col text-center on-hand">85</div>
-          <div className="col text-center">
-            <input type="number" className="form-control" placeholder="Quantity" />
-          </div>
-        </div>
+        ))}
+        {/*  */}
         <div className="row">
           <p>You have placed an order for 76 cases.</p>
         </div>
