0e43920490db386d36104c45be94ca2c8ef5e4b6
“Jessica <jmychalm13@gmail.com>
2023-09-10 09:43:06 -0500

    added working receipt route

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,6 +3,7 @@ import { Login } from "./Login";
 import { OrderConfirm } from "./OrderConfirm";
 import { OrderCreate } from "./OrderCreate";
 import { Routes, Route } from "react-router-dom";
+import { Receipt } from "./Receipt";
 
 export function Content() {
   return (
@@ -11,6 +12,7 @@ export function Content() {
         <Route path="/order_create" element={<OrderCreate />} />
         <Route path="/" element={<Login />} />
         <Route path="/order_confirm/:id" element={<OrderConfirm />} />
+        <Route path="/receipt" element={<Receipt />} />
       </Routes>
       {/* <DailyUsage /> */}
     </div>
