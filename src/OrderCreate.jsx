8b246d0e31a05a0dba1c2247933acf0beb50215a
“Jessica <jmychalm13@gmail.com>
2023-09-11 07:07:59 -0500

    some styling changes to orderCreate

--- src/OrderCreate.jsx
+++ src/OrderCreate.jsx
@@ -30,50 +30,43 @@ export function OrderCreate() {
   useEffect(displayProducts, []);
   return (
     <div className="order-form">
-      <form onSubmit={handleSubmit} className="form-order mt-5 p-2 m-auto rounded">
-        <h1 className="text-center">Create Order</h1>
-        <div className="header-row row">
-          <div className="text-center col border-bottom text-center product-name">
-            <strong>Product Name</strong>
-          </div>
-          <div className="col text-center text-center uom border-bottom">
-            <strong>UOM</strong>
-          </div>
-          <div className="col text-center text-center on-hand border-bottom">
-            <strong>ON-HAND</strong>
-          </div>
-          <div className="col text-center text-center amount-ordered">
-            <strong>Quantity</strong>
-          </div>
+      <form onSubmit={handleSubmit}>
+        <h1 className="text-center m-3">Create Order</h1>
+        <div className="table-responsive d-flex justify-content-center">
+          <table
+            style={{ borderRadius: "5px", overflow: "hidden" }}
+            className="table border border-black table-striped"
+          >
+            <thead className="bg-none">
+              <tr className="text-white">
+                <th>Product Name</th>
+                <th>UOM</th>
+                <th>ON-HAND</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody className="text-95 text-secondary-d3">
+              {products.map((product) => (
+                <tr className="" key={product.id}>
+                  <td>{product.product_name}</td>
+                  <td className="td-number">{product.uom}</td>
+                  <td className="td-number">{product.on_hand}</td>
+                  <td>
+                    <input
+                      name={"quantity" + product.id}
+                      type="number"
+                      className="form-control"
+                      placeholder="0"
+                      onChange={(event) => updateProductsToOrder(product.id, event.target.value)}
+                      max={50}
+                      min={0}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        {products.map((product) => (
-          <div key={product.id} className="row">
-            <div className="border product-name col text-center">
-              <p>{product.product_name}</p>
-            </div>
-            <div className="border col text-center text-center uom">{product.uom}</div>
-            <div className="border col text-center on-hand">{product.on_hand}</div>
-            <div className="col text-center">
-              <input
-                name={"quantity" + product.id}
-                type="number"
-                className="form-control"
-                placeholder="Quantity"
-                onChange={(event) => updateProductsToOrder(product.id, event.target.value)}
-                max={50}
-                min={0}
-              />
-            </div>
-          </div>
-        ))}
-        <div className="d-flex justify-content-end">
-          <button type="submit" className="btn border">
-            Place Order
-          </button>
-        </div>
-        {/* <div className="row">
-          <p>You have placed an order for 76 cases.</p>
-        </div> */}
       </form>
     </div>
   );
