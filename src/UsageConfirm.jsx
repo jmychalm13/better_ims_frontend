f5d5b4a3f2369d2b0a215968b8652b96dc2ffd82
“Jessica <jmychalm13@gmail.com>
2023-09-12 12:52:33 -0500

    dailyUsage table complete

--- src/UsageConfirm.jsx
+++ src/UsageConfirm.jsx
@@ -14,7 +14,7 @@ export function UsageConfirm() {
     axios.get(`http://localhost:3000/daily_usages.json`).then((response) => {
       let items = response.data;
       items.forEach((product) => {
-        products.push(product.product);
+        products.push(product);
       });
       getCurrentProducts(products);
     });
@@ -23,10 +23,32 @@ export function UsageConfirm() {
   useEffect(getCurrentUsage, []);
 
   return (
-    <div>
-      {currentProducts.map((product) => (
-        <p key={product.id}>{product.product_name}</p>
-      ))}
-    </div>
+    <form action="">
+      <h1 className="text-center m-3">Usage Confirmed</h1>
+      <div className="table-responsive d-flex justify-content-center">
+        <table style={{ borderRadius: "5px", overflow: "hidden" }} className="table border border-black table-striped">
+          <thead className="bg-none">
+            <tr className="text-white">
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Qty Used</th>
+            </tr>
+          </thead>
+          <tbody className="text-95 text-secondary-d3">
+            {currentProducts.map((product, index) => (
+              <tr key={product.product.product_name}>
+                <td>{index + 1}</td>
+                <td>{product.product.product_name}</td>
+                <td>{product.quantity_used}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </form>
   );
 }
+
+// {currentProducts.map((product) => (
+//   <p key={product.id}>{product.product_name}</p>
+// ))}
